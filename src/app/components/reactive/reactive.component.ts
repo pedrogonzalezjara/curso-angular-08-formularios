85989b12e33eb9aefc303ccd2ddc1443616dd5d5
Meruem <pdro.gonzalez@gmail.com>
2020-11-01 01:03:25 -0300

    Avance seccion 8 31/10/2020

--- src/app/components/reactive/reactive.component.ts
+++ src/app/components/reactive/reactive.component.ts
@@ -12,21 +12,64 @@ export class ReactiveComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { 
     this.crearFormulario();
+    this.cargarDataFormulario();
   }
 
   ngOnInit() {
+  }
+
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+  get correoNoValido(){
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+  get apellidoNoValido(){
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+  get distritoNoValido(){
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+  get ciudadNoValida(){
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
   }
 
   crearFormulario(){
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', [Validators.required, Validators.minLength(5)]],
-      correo: ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      correo: ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      direccion: this.fb.group({
+        distrito: ['', [Validators.required]],
+        ciudad: ['', [Validators.required]]
+      })
+    })
+  }
+
+  cargarDataFormulario() {
+    this.forma.reset({
+      nombre: "toby",
+      apellido: "toby",
+      correo: "toby@gmail.com",
+      direccion: {
+        distrito: "4",
+        ciudad: "perro"
+      }
     })
   }
 
   guardar(){
-    console.log(this.forma);
+    if(this.forma.invalid){
+      return Object.values(this.forma.controls).forEach(control => {
+        if( control instanceof FormGroup ) {
+          Object.values( control.controls ).forEach( control => control.markAsTouched());
+        } else {
+          control.markAsTouched();
+        }
+      });
+      
+    }
+    this.forma.reset;
   }
 
 }
